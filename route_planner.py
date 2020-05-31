353539f847945c10eacc868cc793dfa7d8917498
Alana Bellucci <fastalana@gmail.com>
2020-05-31 13:07:04 -0600

    Done

--- route_planner.py
+++ route_planner.py
@@ -2,15 +2,15 @@ import heapq
 import math
 from helpers import Map, load_map, show_map
 
-# calculates the shortest path between two points
+# calculates the shortest path between two nodes
 def shortest_path(Map,start,goal): # example inputs Map = map_10, start = 2, goal = 0
-    # tracks teh nodes visited and what node you "came from"
-    # at the end of this function came_from will look like {2: 0, 4: 2, 3: 2, 1: 2, 5: 3, 7: 5, 0: 5} with the inputs above
-    came_from = {}
-    came_from[start] = 0 # {2:0}
+    # tracks the nodes visited and what node you visited prior
+    # at the end of this function node_path will look like {2: 0, 4: 2, 3: 2, 1: 2, 5: 3, 7: 5, 0: 5} with the inputs above
+    node_path = {}
+    node_path[start] = 0 # {2:0}
     
-    cost_so_far = {}
-    cost_so_far[start] = 0 # {2:0}
+    current_cost = {}
+    current_cost[start] = 0 # {2:0}
     
     # min heap that tracks the distances between a start and destination node
     frontier = [(0, start)] # [(0,20)]    
@@ -23,22 +23,22 @@ def shortest_path(Map,start,goal): # example inputs Map = map_10, start = 2, goa
             
         for neighbor in Map.roads[node]: # Map.roads[node] = Map.roads[2] = [4, 3, 1]
             path_cost = distance(Map.intersections[node], Map.intersections[neighbor]) # distance(2, 7) = 0.49619668801776967
-            new_cost = cost_so_far[node] + path_cost # new_cost = cost_so_far[2] + 0.49619668801776967 = 0.49619668801776967
+            new_cost = current_cost[node] + path_cost # new_cost = current_cost[2] + 0.49619668801776967 = 0.49619668801776967
             
-            if neighbor not in cost_so_far or new_cost < cost_so_far[neighbor]:
-                came_from[neighbor] = node  # {2: 0, 7: 2}
-                cost_so_far[neighbor] = new_cost # {2: 0, 7: 0.49619668801776967}
+            if neighbor not in current_cost or new_cost < current_cost[neighbor]:
+                node_path[neighbor] = node  # {2: 0, 7: 2}
+                current_cost[neighbor] = new_cost # {2: 0, 7: 0.49619668801776967}
                 heapq.heappush(frontier, (new_cost, neighbor)) # frontier (once every neighbor has been visited) = [(0.128, 3), (0.212, 4), (0.134, 1)]
 
-    return best_route(came_from, start, goal)
+    return best_route(node_path, start, goal)
 
 # calculates the distance between two nodes
 def distance(start, end):
         return (math.hypot(end[0] - start[0], end[1] - start[1]))
 
 # traverse backwards to find the optimal path
-def best_route(came_from, start, goal):
-    if goal not in came_from:
+def best_route(node_path, start, goal):
+    if goal not in node_path:
         return(f"Goal destination {goal} not found on map.")
     
     node = goal
@@ -46,7 +46,7 @@ def best_route(came_from, start, goal):
     
     while node != start:
         path.append(node)
-        node = came_from[node] 
+        node = node_path[node] 
     path.append(start)
     path.reverse()
     return path
