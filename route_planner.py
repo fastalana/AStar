46bc2210996175923519c817c7340607e7ceac73
Alana Bellucci <fastalana@gmail.com>
2020-05-31 12:08:52 -0600

    WIP

--- route_planner.py
+++ route_planner.py
@@ -3,31 +3,31 @@ import math
 from helpers import Map, load_map, show_map
 
 # calculates the shortest path between two points
-def shortest_path(Map,start,goal):
+def shortest_path(Map,start,goal): # example inputs Map = map_10, start = 2, goal = 0
     came_from = {}
-    came_from[start] = 0
+    came_from[start] = 0 # {2:0}
     
     cost_so_far = {}
-    cost_so_far[start] = 0
+    cost_so_far[start] = 0 # {2:0}
     
-    frontier = [(0, start)]
+    frontier = [(0, start)] # [(0,20)]    
     
     while len(frontier) > 0:
-        node = heapq.heappop(frontier)[1]
+        node = heapq.heappop(frontier)[1] # first iteration, node = 2
         
         if node == goal:
             break
             
-        for neighbor in Map.roads[node]:
-            path_cost = distance(Map.intersections[node], Map.intersections[neighbor])
-            new_cost = cost_so_far[node] + path_cost
+        for neighbor in Map.roads[node]: # Map.roads[node] = Map.roads[2] = [4, 3, 1]
+            path_cost = distance(Map.intersections[node], Map.intersections[neighbor]) # distance(2, 7) = 0.49619668801776967
+            new_cost = cost_so_far[node] + path_cost # new_cost = cost_so_far[2] + 0.49619668801776967 = 0.49619668801776967
             
             if neighbor not in cost_so_far or new_cost < cost_so_far[neighbor]:
-                came_from[neighbor] = node
-                cost_so_far[neighbor] = new_cost
-                heapq.heappush(frontier, (new_cost, neighbor))
+                came_from[neighbor] = node  # {2: 0, 7: 2}
+                cost_so_far[neighbor] = new_cost # {2: 0, 7: 0.49619668801776967}
+                heapq.heappush(frontier, (new_cost, neighbor)) # frontier (once every neighbor has been visited) = [(0.128, 3), (0.212, 4), (0.134, 1)]
     
-#     print(came_from, cost_so_far)
+
     return best_route(came_from, start, goal)
 
 # calculates the distance between two points
@@ -49,18 +49,8 @@ def best_route(came_from, start, goal):
     path.reverse()
     return path
 
-### TEST MAPS ###
-map_10 = load_map("map-10.pickle")
-map_40 = load_map("map-40.pickle")
+### TEST MAP ###
+# map_10 = load_map("map-10.pickle")
 
-
-# print("map_10 intersections", map_10.intersections)
-# print("map_40 intersections", map_40.intersections)
-
-# # test 1
-# shortest_path(map_40, 8, 24)
-# # path: [8, 14, 16, 37, 12, 17, 10, 24]
-
-# # test 2
 # shortest_path(map_10, 2, 0)
 # # path: [2, 3, 5, 0]
