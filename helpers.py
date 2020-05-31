f79e1d7fa4e6267a2f003d66bc045ca077930265
Alana Bellucci <fastalana@gmail.com>
2020-05-31 09:13:17 -0600

    WIP

--- helpers.py
+++ helpers.py
@@ -4,65 +4,68 @@ import plotly.plotly as py
 import random
 from plotly.graph_objs import *
 from plotly.offline import init_notebook_mode, plot, iplot
+
 init_notebook_mode(connected=True)
 
-class Map:
-	def __init__(self, G):
-		self._graph = G
-		self.intersections = nx.get_node_attributes(G, "pos")
-		self.roads = [list(G[node]) for node in G.nodes()]
 
-	def save(self, filename):
-		with open(filename, 'wb') as f:
-			pickle.dump(self._graph, f)
+class Map:
+    def __init__(self, G):
+        self._graph = G
+        self.intersections = nx.get_node_attributes(G, "pos")
+        self.roads = [list(G[node]) for node in G.nodes()]
+
+    def save(self, filename):
+        with open(filename, "wb") as f:
+            pickle.dump(self._graph, f)
+
 
 def load_map(name):
-	with open(name, 'rb') as f:
-		G = pickle.load(f)
-	return Map(G)
+    with open(name, "rb") as f:
+        G = pickle.load(f)
+    return Map(G)
+
 
 def show_map(M, start=None, goal=None, path=None):
     G = M._graph
-    pos = nx.get_node_attributes(G, 'pos')
+    pos = nx.get_node_attributes(G, "pos")
     edge_trace = Scatter(
-    x=[],
-    y=[],
-    line=Line(width=0.5,color='#888'),
-    hoverinfo='none',
-    mode='lines')
+        x=[], y=[], line=Line(width=0.5, color="#888"), hoverinfo="none", mode="lines"
+    )
 
     for edge in G.edges():
-        x0, y0 = G.node[edge[0]]['pos']
-        x1, y1 = G.node[edge[1]]['pos']
-        edge_trace['x'] += [x0, x1, None]
-        edge_trace['y'] += [y0, y1, None]
+        x0, y0 = G.node[edge[0]]["pos"]
+        x1, y1 = G.node[edge[1]]["pos"]
+        edge_trace["x"] += [x0, x1, None]
+        edge_trace["y"] += [y0, y1, None]
 
     node_trace = Scatter(
         x=[],
         y=[],
         text=[],
-        mode='markers',
-        hoverinfo='text',
+        mode="markers",
+        hoverinfo="text",
         marker=Marker(
             showscale=False,
             # colorscale options
             # 'Greys' | 'Greens' | 'Bluered' | 'Hot' | 'Picnic' | 'Portland' |
             # Jet' | 'RdBu' | 'Blackbody' | 'Earth' | 'Electric' | 'YIOrRd' | 'YIGnBu'
-            colorscale='Hot',
+            colorscale="Hot",
             reversescale=True,
             color=[],
             size=10,
             colorbar=dict(
                 thickness=15,
-                title='Node Connections',
-                xanchor='left',
-                titleside='right'
+                title="Node Connections",
+                xanchor="left",
+                titleside="right",
             ),
-            line=dict(width=2)))
+            line=dict(width=2),
+        ),
+    )
     for node in G.nodes():
-        x, y = G.node[node]['pos']
-        node_trace['x'].append(x)
-        node_trace['y'].append(y)
+        x, y = G.node[node]["pos"]
+        node_trace["x"].append(x)
+        node_trace["y"].append(y)
 
     for node, adjacencies in enumerate(G.adjacency_list()):
         color = 0
@@ -73,19 +76,21 @@ def show_map(M, start=None, goal=None, path=None):
         elif node == goal:
             color = 1
         # node_trace['marker']['color'].append(len(adjacencies))
-        node_trace['marker']['color'].append(color)
+        node_trace["marker"]["color"].append(color)
         node_info = "Intersection " + str(node)
-        node_trace['text'].append(node_info)
+        node_trace["text"].append(node_info)
 
-    fig = Figure(data=Data([edge_trace, node_trace]),
-                 layout=Layout(
-                    title='<br>Network graph made with Python',
-                    titlefont=dict(size=16),
-                    showlegend=False,
-                    hovermode='closest',
-                    margin=dict(b=20,l=5,r=5,t=40),
-                   
-                    xaxis=XAxis(showgrid=False, zeroline=False, showticklabels=False),
-                    yaxis=YAxis(showgrid=False, zeroline=False, showticklabels=False)))
+    fig = Figure(
+        data=Data([edge_trace, node_trace]),
+        layout=Layout(
+            title="<br>Network graph made with Python",
+            titlefont=dict(size=16),
+            showlegend=False,
+            hovermode="closest",
+            margin=dict(b=20, l=5, r=5, t=40),
+            xaxis=XAxis(showgrid=False, zeroline=False, showticklabels=False),
+            yaxis=YAxis(showgrid=False, zeroline=False, showticklabels=False),
+        ),
+    )
 
     iplot(fig)
