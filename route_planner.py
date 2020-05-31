28095a3ef39200ea4d36d9869bfdcf59b57a48a2
Alana Bellucci <fastalana@gmail.com>
2020-05-31 10:36:53 -0600

    Working

--- route_planner.py
+++ route_planner.py
@@ -1,3 +1,4 @@
+import heapq
 import math
 from helpers import Map, load_map, show_map
 
@@ -11,13 +12,29 @@ def shortest_path(Map,start,goal):
     
     frontier = [(0, start)]
     
-    return came_from
-#     return best_route(came_from, start, goal)
+    while len(frontier) > 0:
+        node = heapq.heappop(frontier)[1]
+        
+        if node == goal:
+            break
+            
+        for neighbor in Map.roads[node]:
+            path_cost = distance(Map.intersections[node], Map.intersections[neighbor])
+            new_cost = cost_so_far[node] + path_cost
+            
+            if neighbor not in cost_so_far or new_cost < cost_so_far[neighbor]:
+                came_from[neighbor] = node
+                cost_so_far[neighbor] = new_cost
+                heapq.heappush(frontier, (new_cost, neighbor))
+    
+#     print(came_from, cost_so_far)
+    return best_route(came_from, start, goal)
 #     return("shortest path called.")
 
 # calculates the distance between two points
 def distance(start, end):
-    return (math.hypot(map_10.intersections[end][0] - map_10.intersections[start][0], map_10.intersections[end][1] - map_10.intersections[start][1]))
+#     return (math.hypot(map_10.intersections[end][0] - map_10.intersections[start][0], map_10.intersections[end][1] - map_10.intersections[start][1]))
+        return (math.hypot(end[0] - start[0], end[1] - start[1]))
 
 # traverse backwards to find the optimal path
 def best_route(came_from, start, goal):
@@ -38,7 +55,7 @@ def best_route(came_from, start, goal):
 
 ### TEST MAPS ###
 map_10 = load_map("map-10.pickle")
-# map_40 = load_map("map-40.pickle")
+map_40 = load_map("map-40.pickle")
 
 
 # print("map_10 intersections", map_10.intersections)
@@ -51,9 +68,3 @@ map_10 = load_map("map-10.pickle")
 # # test 2
 # shortest_path(map_10, 2, 0)
 # # path: [2, 3, 5, 0]
-
-
-# # test 3
-# shortest_path(map_10, 3, 9)
-# # path: []
-
