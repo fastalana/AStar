f72c95643d9776f247e1624a2845af2f68f1ab70
Alana Bellucci <fastalana@gmail.com>
2020-05-31 09:12:28 -0600

    WIP

--- student_code.py
+++ student_code.py
@@ -1,5 +1,11 @@
+from helpers import Map, load_map, show_map
+
 def shortest_path(M,start,goal):
     return ("shortest path called")
 
 
 print(shortest_path(1, 2, 3,))
+
+### TEST MAPS ###
+# map_10 = load_map("map-10.pickle")
+# map_40 = load_map("map-4.pickle")
